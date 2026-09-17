56608a0bebf69dc8a309da456d63acb22101f292
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2026-09-18 04:23:58 +0530

    Add Promise.all example with multiple promises

--- Promise Api[all,allsettle,race,any]/promiseAll.js
+++ Promise Api[all,allsettle,race,any]/promiseAll.js
@@ -1,1 +1,24 @@
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P1 success");
+  }, 3000);
+});
 
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P2 success");
+  }, 1000);
+});
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P3 success");
+  }, 2000);
+});
+Promise.all([p1, p2, p3]).then((res) => {
+  console.log(res);
+});
+
+
+// OUTPUT
+// [ 'P1 success', 'P2 success', 'P3 success' ]
