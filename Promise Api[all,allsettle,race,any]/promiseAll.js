5226e9fd04fe5c944f0cadc6a7353b1666ff5c63
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2026-09-18 04:39:31 +0530

    Update promiseAll.js

--- Promise Api[all,allsettle,race,any]/promiseAll.js
+++ Promise Api[all,allsettle,race,any]/promiseAll.js
@@ -21,4 +21,40 @@ Promise.all([p1, p2, p3]).then((res) => {
 
 
 // OUTPUT
-// [ 'P1 success', 'P2 success', 'P3 success' ]
+// [ 'P1 success', 'P2 success', 'P3 success' ]     -after 3 second
+
+
+// If p2 failed
+
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P1 success");
+  }, 3000);
+});
+
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej("P2 failed");
+  }, 1000);
+});
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P3 success");
+  }, 2000);
+});
+Promise.all([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+
+
+
+// OUTPUT
+// P2 failed      -after 1 second
+
+
