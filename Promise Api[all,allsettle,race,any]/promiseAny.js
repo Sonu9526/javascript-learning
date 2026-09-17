66c705b74e1322213857c68b97381d238d3a6d6e
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2026-09-18 05:23:22 +0530

    Clarify Promise.any() behavior in comments
    Updated comments to clarify the behavior of Promise.any() with fulfilled and rejected promises.

--- Promise Api[all,allsettle,race,any]/promiseAny.js
+++ Promise Api[all,allsettle,race,any]/promiseAny.js
@@ -1,5 +1,5 @@
 // Promise.any() returns the result of the first fulfilled Promise.
-
+//  If all Promises are rejected, Promise.any() rejects with an AggregateError containing all the rejection reasons. ✅
 
 const p1 = new Promise((res, rej) => {
   setTimeout(() => {
@@ -32,7 +32,6 @@ Promise.any([p1, p2, p3])
 
 
 
-
 const p1 = new Promise((res, rej) => {
   setTimeout(() => {
     rej("P1 failed");
@@ -60,10 +59,45 @@ Promise.any([p1, p2, p3])
 
 
 
+// OUTPUT
+// P2 success    --it wait for 5 second because the other two promise is rejected Promise.any() only return the fullfiled promise
+
+
+
+// If all the promise rejected
+
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej("P1 failed");
+  }, 2000);
+});
+
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej("P2 failed");
+  }, 5000);
+});
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej("P3 failed");
+  }, 1000);
+});
+Promise.any([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 
 
 // OUTPUT
-// P2 success    --it wait for 5 second because the other two promise is rejected Promise.any() only return the fullfiled promise
+
+// [AggregateError: All promises were rejected] {
+//   [errors]: [ 'P1 failed', 'P2 failed', 'P3 failed' ]         --if the all promise is rejected promise.any return a aggregateError with all the rejected result
+// }
 
 
 
