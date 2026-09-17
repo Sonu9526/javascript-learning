c9a2bf3716de13e1b049b13122cf636e90f4ce43
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2026-09-18 04:57:36 +0530

    Add promiseRace.js to demonstrate Promise.race
    Implement Promise.race with multiple promises and log results.

---
+++ Promise Api[all,allsettle,race,any]/promiseRace.js
@@ -0,0 +1,67 @@
+// Returns the first settled Promise — fulfilled (use fulfilled, not resolved, because a Promise can settle as either fulfilled or rejected.) or rejected.  
+
+
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P1 success");
+  }, 1000);
+});
+
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej("P2 failed");
+  }, 2000);
+});
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P3 success");
+  }, 2000);
+});
+Promise.race([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+
+// OUTPUT
+// P1 success       --after 1 second
+
+
+
+
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P1 success");
+  }, 2000);
+});
+
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej("P2 failed");
+  }, 1000);
+});
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("P3 success");
+  }, 2000);
+});
+Promise.race([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+
+
+// OUTPUT
+// P2 success       --after 1 second
+
+
+
