b4b54db54efb61fcabb28a04afbc35bfd3ead867
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2026-09-19 00:17:06 +0530

    Update code.js

--- Async and Await/code.js
+++ Async and Await/code.js
@@ -2,7 +2,9 @@ const p = new Promise((res, rej) => {
   res("Promise Resolved value");
 });
 async function handlePromise() {
+  // js engine was waiting for promise for resolve
   const val = await p;
+   console.log("hello");
   console.log(val);
 }
 handlePromise();
